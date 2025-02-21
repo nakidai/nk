0fb0f47e8bcc030ef989142927c56235606e4db7
Nakidai <nakidai@disroot.org>
2025-02-21 22:52:06 +0300

    Implement that using next()

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 partial = __import__("functools").partial
 repeat = __import__("itertools").repeat
 starmap = __import__("itertools").starmap
-res = {}.get(0)
-any(
-    starmap(
-        partial(eval, """
-{
-    "": partial(exec, "exit(0)"),
-    "x": partial(eval, "bool(res := 'x')"),
-    "o": partial(eval, "bool(res := 'o')"),
-}.get(input("x/o? "), type({}.get(0)))()
-"""),
-        repeat(())
+res = next(
+    filter(
+        bool,
+        starmap(
+            partial(
+                eval,
+                '{'
+                '    "": partial(exec, "exit(0)"),'
+                '    "x": partial(eval, "\'x\'"),'
+                '    "o": partial(eval, "\'o\'"),'
+                '}.get(input("x/o? "), type({}.get(0)))()'
+            ),
+            repeat(())
+        ),
     ),
+    False,
 )
 print(res)
