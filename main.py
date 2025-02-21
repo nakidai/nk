6876155e2ae71ca15f1a94c4c646464738c31a6e
Nakidai <nakidai@disroot.org>
2025-02-22 00:45:18 +0300

    Make this OOPish

--- main.py
+++ main.py
@@ -1,21 +1,91 @@
 partial = __import__("functools").partial
 repeat = __import__("itertools").repeat
 starmap = __import__("itertools").starmap
-res = next(
-    filter(
-        bool,
-        starmap(
-            partial(
-                eval,
-                '{'
-                '    "": partial(exec, "exit(0)"),'
-                '    "x": partial(eval, "\'x\'"),'
-                '    "o": partial(eval, "\'o\'"),'
-                '}.get(input("x/o? "), type({}.get(0)))()'
-            ),
-            repeat(())
-        ),
-    ),
-    False,
+function_builder = type(__import__("timeit").timeit)
+
+
+create_namespace = partial(
+    function_builder,
+    compile('', '', 'exec'),
+    {}
 )
-print(res)
+
+
+Side = partial(eval, """
+(
+    self := create_namespace(),
+    exec(
+        '''if 1:
+            self.choose = partial(
+                next,
+                filter(
+                    bool,
+                    starmap(
+                        partial(
+                            eval,
+                            '{'
+                            '    "": partial(exec, "exit(0)"),'
+                            '    "x": partial(eval, "\\\\'x\\\\'"),'
+                            '    "o": partial(eval, "\\\\'o\\\\'"),'
+                            '}.get(input("x/o? "), type({}.get(0)))()'
+                        ),
+                        repeat(())
+                    ),
+                ),
+                False,
+            )
+
+            self.str = self.choose()
+
+            self.switch = partial(
+                exec,
+                'self.str = {"x": "o", "o": "x"}.get(self.str)',
+                locals={"self": self},
+            )
+        '''
+    ),
+    self,
+)[-1]
+""")
+
+
+Board = partial(eval, """
+(
+    self := create_namespace(),
+    exec(
+        '''if 1:
+            self.field = [
+                ['_', '_', '_'],
+                ['_', '_', '_'],
+                ['_', '_', '_'],
+            ]
+
+            self.str = partial(
+                eval,
+                "'\\\\\\\\n'.join(map(''.join, self.field))",
+                locals={"self": self},
+            )
+        '''
+    ),
+    self,
+)[-1]
+""")
+
+
+side = Side()
+print("you've chosen", side.str)
+
+board = Board()
+
+any(
+    starmap(
+        partial(
+            exec,
+            """if 1:
+                print(f"{side.str}\\n{board.str()}")
+                side.switch()
+            """
+        ),
+        repeat((), 5),
+    )
+)
